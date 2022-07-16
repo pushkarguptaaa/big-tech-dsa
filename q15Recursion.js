ba3d7e45eceb23ba13de8677ee04685771e63f7f
pushkarguptaaa <pushkardps8@gmail.com>
2022-07-16 14:58:26 +0530

    Solved recursion  problem

---
+++ q15Recursion.js
@@ -0,0 +1,62 @@
+// NAIVE SOLUTION
+const array = [5, 3, 1, 6, 4, 2];
+const kToFind = 4;
+
+const swap = function (array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+};
+
+const getPartition = function (nums, left, right) {
+  const pivotElement = nums[right];
+  let partitionIdx = left;
+
+  for (let j = left; j < right; j++) {
+    if (nums[j] <= pivotElement) {
+      swap(nums, partitionIdx, j);
+      partitionIdx++;
+    }
+  }
+  swap(nums, partitionIdx, right);
+
+  return partitionIdx;
+};
+
+const quickSort = function (nums, left, right) {
+  if (left < right) {
+    const partitionIndex = getPartition(nums, left, right);
+
+    quickSort(nums, left, partitionIndex - 1);
+    quickSort(nums, partitionIndex + 1, right);
+  }
+};
+
+var findKthLargest = function (nums, k) {
+  const indexToFind = nums.length - k;
+  quickSort(nums, 0, nums.length - 1);
+  return nums[indexToFind];
+};
+
+console.log(findKthLargest(array, kToFind));
+
+// OPTIMISED SOLUTION
+const quickSelect = function (nums, left, right, indexToFind) {
+  const partitionIndex = getPartition(nums, left, right);
+
+  if (partitionIndex === indexToFind) {
+    return nums[partitionIndex];
+  } else if (indexToFind < partitionIndex) {
+    return quickSelect(nums, left, partitionIndex - 1, indexToFind);
+  } else {
+    return quickSelect(nums, partitionIndex + 1, right, indexToFind);
+  }
+};
+
+var findKthLargest1 = function (nums, k) {
+  const indexToFind = nums.length - k;
+
+  return quickSelect(nums, 0, nums.length - 1, indexToFind);
+};
+
+console.log(findKthLargest1(array, kToFind));
